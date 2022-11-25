407f25d5266b7ca69f578d8d7abd1dd769853eb0
Charlotte Hank <charlotte.hank@vanderbilt.edu>
2022-11-25 16:04:18 -0600

    initial commit

---
+++ laragigs/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Listing;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory(5)->create();
+
+        Listing::factory(6)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
